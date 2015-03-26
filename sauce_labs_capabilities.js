0dd26e9b055ab0920717a5c23ccd5597158bac6e
Aleksandar Djindjic <djindjic@gmail.com>
2015-03-26 10:37:21 +0100

    setup shippable

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -6,20 +6,20 @@ module.exports.capabilities = {
     browserName: 'firefox',
     platform: 'Linux',
     version: '14',
-    name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
+    name: pkg.name + ' (build: ' + process.env.TRAVIS_BUILD_NUMBER + ')'
   },
   sl_ios_safari: {
     base: 'SauceLabs',
     browserName: 'safari',
     platform: 'OS X 10.8',
     version: '6',
-    name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
+    name: pkg.name + ' (build: ' + process.env.TRAVIS_BUILD_NUMBER + ')'
   }//,
   // sl_ie_9: {
   //   base: 'SauceLabs',
   //   browserName: 'internet explorer',
   //   platform: 'Windows 7',
   //   version: '9',
-  //   name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
+  //   name: pkg.name + ' (build: ' + process.env.TRAVIS_BUILD_NUMBER + ')'
   // }
 };
