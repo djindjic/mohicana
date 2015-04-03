68c1a0ab159fa294563562deb798afe147e5e497
Aleksandar Djindjic <djindjic@gmail.com>
2015-04-03 09:09:01 +0200

    coverage

--- karma.conf.js
+++ karma.conf.js
@@ -22,21 +22,20 @@ module.exports = function(config) {
     files: [
     ],
 
+    preprocessors: {
+      'lib/**/*.js': ['coverage']
+    },
+    reporters: ['progress', 'coverage'],
 
     // list of files to exclude
     exclude: [
     ],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    // reporters: ['mocha'],
 
 
     // web server port
